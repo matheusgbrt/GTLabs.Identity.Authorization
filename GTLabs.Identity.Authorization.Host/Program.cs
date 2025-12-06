5b019cb337189f8cbf6d0b1b9bab89778b1883ae
Matheus Guedes Bertaiolli <70043048+matheusgbrt@users.noreply.github.com>
2025-12-06 00:04:21 -0300

    feat: progression

--- GTLabs.Identity.Authorization.Host/Program.cs
+++ GTLabs.Identity.Authorization.Host/Program.cs
@@ -1,9 +1,8 @@
-using Gtlabs.Api.AmbientData;
 using Gtlabs.Api.Extensions;
 using Gtlabs.AppRegistration.Extensions;
 using Gtlabs.AspNet.Extensions;
+using Gtlabs.Authentication.Extensions;
 using Gtlabs.Consul.Extensions;
-using Gtlabs.DependencyInjections.DependencyInjectons.Extensions;
 using GTLabs.Identity.Authorization.Infrastructure.Contexts;
 using Gtlabs.Persistence.Extensions;
 using Gtlabs.Redis.Extensions;
@@ -12,7 +11,6 @@ using Gtlabs.ServiceBus.ServiceBus.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.RegisterApp("GTLabs.Identity.Authorization");
-builder.Services.AddHttpContextAccessor();
 builder.Services.AddBasicFeatures();
 await builder.Configuration.AddConsulConfigurationAsync();
 builder.Services.AddConsulRegistration(builder.Configuration);
@@ -21,17 +19,12 @@ builder.Services.RegisterServiceBus(builder.Configuration);
 builder.Services.AddRedisCache(builder.Configuration);
 
 builder.Services.AddPersistence<AuthorizationDbcontext>(builder.Configuration);
+builder.Services.UseAuthenticationServiceHeader(options => options.UseAuthHeader = true);
 
-
-builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 app.AddConsulHealthCheck();
